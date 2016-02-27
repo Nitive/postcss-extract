33ea7bea60e6d6c31a3f6979d54dc040152427aa
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-02-28 00:46:30 +0600

    Add test extracted files

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ import nesting from 'postcss-nesting'
 import plugin from './'
 
 
-function run(done, input, output, opts = {}) {
+const run = (done, input, output, opts = {}) => {
   return postcss([nesting({ bubble: 'important' }), plugin(opts)])
     .process(input)
     .then(result => {
@@ -18,21 +18,30 @@ function run(done, input, output, opts = {}) {
     .catch(done)
 }
 
-const options = {
-  extract: {
-    important: 'extracted.css',
-  },
+
+const getFileContent = (dir, file) => {
+  return fs.readFileSync(path.join(dir, file)).toString().trim()
 }
+
 
 const fixturesDir = path.join(__dirname, 'fixtures')
 fs.readdirSync(fixturesDir).forEach(caseName => {
   if (caseName.includes('[notest]')) return
 
+  const options = {
+    extract: {
+      important: `fixtures/${caseName}/extracted-actual.css`,
+    },
+  }
+
   const fixtureDir = path.join(fixturesDir, caseName)
   if (!fs.lstatSync(fixtureDir).isDirectory()) return
   it(caseName, done => {
-    const input = fs.readFileSync(path.join(fixtureDir, 'input.css')).toString()
-    const output = fs.readFileSync(path.join(fixtureDir, 'output.css')).toString()
+    const input = getFileContent(fixtureDir, 'input.css')
+    const output = getFileContent(fixtureDir, 'output.css')
+    const extracted = getFileContent(fixtureDir, 'extracted.css')
+    const extractedActual = getFileContent(fixtureDir, 'extracted-actual.css')
+    extracted.should.be.equal(extractedActual)
     run(done, input, output, options)
   })
 })

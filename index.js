fef970dc759e8c279597ea3e34bbb64ce5f88821
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-02-27 23:59:39 +0600

    Add extracting

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 /* eslint-disable no-unused-vars */
+
 import 'babel-polyfill'
 import postcss from 'postcss'
+import path from 'path'
+import fs from 'fs'
+
+
+const fromHere = relativePath => {
+  return path.join(__dirname, relativePath)
+}
+
+
+const getRoot = atRule => {
+  let rule = atRule
+  while (rule.parent) {
+    rule = rule.parent
+  }
+  return rule
+}
+
 
 export default postcss.plugin('postcss-extract', (options = {}) => {
   return (css, result) => {
-    Object.keys(options.extract).forEach(atRule => {
+    const atRules = options.extract
+    Object.keys(atRules).forEach(atRule => {
+      const extracted = postcss.root()
       css.walkAtRules(atRule, rule => {
+        extracted.append(getRoot(rule))
         rule.remove()
       })
+
+      // clean all rules except rules inside our at-rule
+      extracted.walkRules(rule => {
+        if (rule.parent.name === atRule) return
+        rule.remove()
+      })
+
+      // remove at-rule wrapper
+      extracted.walkAtRules(atRule, rule => {
+        rule.replaceWith(rule.nodes)
+      })
+
+      fs.writeFileSync(`${atRules[atRule]}`, extracted.toResult().css)
     })
   }
 })
